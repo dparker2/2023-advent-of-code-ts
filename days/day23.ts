c6d168a0ec4530f828ec8c16ee785a1b3e6118cc
dparker2 <crazdave@gmail.com>
2023-12-28 14:59:14 -0800

    finish misery hike

--- days/day23.ts
+++ days/day23.ts
@@ -1,53 +1,78 @@
+type WeightedAdjList = Record<string, Record<string, number>>;
+type Path = { nodes: string[]; weight: number };
+
 function toAdjacencyList(lines: string[]) {
-  const graph: Record<string, string[]> = {};
+  const graph: WeightedAdjList = {};
   for (let i = 0; i < lines.length; i++) {
     for (let j = 0; j < lines[i].length; j++) {
       const char = lines[i][j];
       if (char === "#") continue;
       const k = `${i},${j}`;
-      graph[k] = [];
+      graph[k] = {};
       if ([lines[i - 1]?.[j], char].every((ch) => ch === "." || ch === "^"))
-        graph[k].push(`${i - 1},${j}`);
+        graph[k][`${i - 1},${j}`] = 1;
       if ([lines[i + 1]?.[j], char].every((ch) => ch === "." || ch === "v"))
-        graph[k].push(`${i + 1},${j}`);
+        graph[k][`${i + 1},${j}`] = 1;
       if ([lines[i]?.[j - 1], char].every((ch) => ch === "." || ch === "<"))
-        graph[k].push(`${i},${j - 1}`);
+        graph[k][`${i},${j - 1}`] = 1;
       if ([lines[i]?.[j + 1], char].every((ch) => ch === "." || ch === ">"))
-        graph[k].push(`${i},${j + 1}`);
+        graph[k][`${i},${j + 1}`] = 1;
     }
   }
   return graph;
 }
 
-function topSort(graph: Record<string, string[]>, start: string) {
-  const seen = new Set<string>();
-  const keys: string[] = [];
-  const dfs = (key: string) => {
-    seen.add(key);
-    graph[key].forEach((k) => {
-      if (!seen.has(k)) dfs(k);
-    });
-    keys.push(key);
-  };
-  dfs(start);
-  return keys.reverse();
+function compress(graph: WeightedAdjList) {
+  for (let node in graph) {
+    const neighbors = Object.keys(graph[node]);
+    if (
+      neighbors.length === 2 &&
+      graph[neighbors[0]][node] &&
+      graph[neighbors[1]][node]
+    ) {
+      // O<->O<->O
+      //     ^ Eliminate this node
+      const [n1, n2] = neighbors;
+      graph[n1][n2] = graph[n1][node] + graph[node][n2];
+      graph[n2][n1] = graph[n2][node] + graph[node][n1];
+      delete graph[node];
+      delete graph[n1][node];
+      delete graph[n2][node];
+    }
+  }
+}
+
+function longestUniquePath(graph: WeightedAdjList, start: string) {
+  const queue: Path[] = [{ nodes: [start], weight: 0 }];
+  const paths: Record<string, Path> = {};
+  for (let node of Object.keys(graph)) paths[node] = { nodes: [], weight: 0 };
+
+  while (queue.length) {
+    const path = queue.shift()!;
+    const curr = path.nodes.at(-1)!;
+    if (path.weight > paths[curr].weight) paths[curr] = path;
+
+    for (let neighbor in graph[curr]) {
+      if (path.nodes.includes(neighbor)) continue;
+      queue.push({
+        nodes: [...path.nodes, neighbor],
+        weight: path.weight + graph[curr][neighbor],
+      });
+    }
+  }
+
+  return paths;
 }
 
 export function part1(input: string) {
   const lines = input.split("\n");
   const graph = toAdjacencyList(lines);
+  compress(graph);
   const start = "0,1";
   const goal = `${lines.length - 1},${lines[lines.length - 1].length - 2}`;
-  const keys = topSort(graph, start);
+  const paths = longestUniquePath(graph, start);
 
-  const T: Record<string, number> = {};
-  for (let i = 0; i < keys.length; i++) T[keys[i]] = 0;
-  for (let i = 0; i < keys.length; i++)
-    graph[keys[i]].forEach((k) => {
-      if (T[k] === undefined || T[k] < T[keys[i]] + 1) T[k] = T[keys[i]] + 1;
-    });
-
-  return T[goal];
+  return paths[goal].weight;
 }
 
 export function part2(input: string) {
